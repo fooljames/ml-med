670fabb1cb9863538b67c4ac6f7122adc98f938d
James S <sorawitj@gmail.com>
2017-11-09 14:05:18 +0700

    add utils and columns_id_names mapping

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+
+location_date = ['dataset_location', 'dataset_datetime']
+
 def mean_by_interval(df, interval):
     """
     mean ignore NULL value
@@ -6,16 +10,7 @@ def mean_by_interval(df, interval):
     :return: aggregated dataframe
     """
 
-
-def filter_duplicate(df):
-    """
-    filter out duplicated rows
-    :param df: 
-    :return: 
-    """
-
-
-def change_col_names(df):
+def load_data(data_path, colname_path):
     """
     change column's name to a be readable one
     measurement -> name_measure
@@ -24,6 +19,32 @@ def change_col_names(df):
     :param df: 
     :return: 
     """
+    ## load data and colnames files
+    data = pd.read_csv(data_path)
+    colnames = pd.read_csv(colname_path)
+
+    ## get only relevant columns
+    selected_cols = data.columns[data.columns.isin(colnames.id)]
+    data = data[location_date + list(selected_cols)]
+
+    ## drop duplicates
+    data = data.drop_duplicates(location_date + list(selected_cols))
+
+    ## get mapping from id to colname
+    colnames.index = colnames.id
+    name_dict = colnames['name'].to_dict()
+
+    ## change columns into names
+    data.columns = location_date + [name_dict[c] for c in selected_cols]
+
+    return data
+
+
+def separate_type(df):
+    # divide data into settings and measurements
+    settings = df[location_date + list(df.columns[df.columns.str.contains("Setting")])]
+    measurement = df[list(df.columns[~df.columns.str.contains("Setting")])]
+    return settings, measurement
 
 
 def find_consecutive_null(df):
